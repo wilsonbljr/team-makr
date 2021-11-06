aa3c6d52dd8b86e0cfa5efea14fbb68845c2393b
wilsonbljr <wilsonbljr@gmail.com>
2021-11-05 21:24:21 -0300

    Updates EER Diagram

--- database-docker/Seeds/dropseeds.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM person_to_hard_skill;
-DELETE FROM person_to_soft_skill;
-DELETE FROM person_to_team;
-DELETE FROM hard_skill;
-DELETE FROM log;
-DELETE FROM person;
-DELETE FROM soft_skill;
-DELETE FROM team;

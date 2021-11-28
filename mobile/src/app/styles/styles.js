6faa55eecff5d678842ecdf4d3782a5ca53f305e
Wilson Bley <wilsonbljr@gmail.com>
2021-11-28 12:13:59 -0300

    Adds global styles

---
+++ mobile/src/app/styles/styles.js
@@ -0,0 +1,12 @@
+export const primaryColour = '#30415d';
+export const lightPrimaryColour = '#8eaebd';
+export const secondaryColour = '#cf6766';
+export const backgroundColour = '#031424';
+export const iconColor = '#FFF';
+
+export const fontFamilyLight = 'Montserrat-Light';
+export const fontFamilyRegular = 'Montserrat-Regular';
+export const fontFamilyMedium = 'Montserrat-Medium';
+export const fontFamilyBold = 'Montserrat-Bold';
+
+export const fontSizeLg = 28;
